dcc61e9cc3142977bd4bb0069b6ef1bc603f849a
Meche-Ann McKenzie <meche.ann.mckenzie@gmail.com>
2025-07-05 03:58:02 -0400

    updated getItemsFromTotalCost

--- src/ConferenceEvent.jsx
+++ src/ConferenceEvent.jsx
@@ -53,6 +53,27 @@ const ConferenceEvent = () => {
 
     const getItemsFromTotalCost = () => {
         const items = [];
+        venueItems.forEach((item) => {
+            if (item.quantity > 0)
+                items.push({ ...item, type: "venue" });
+        });
+
+        avItems.forEach((item) => {
+            if (item.quantity > 0 && !item.some((i) => i.name === item.name && i.type === "av")) {
+                items.push({ ...item, type: "av" });
+            }
+        });
+
+        mealItems.forEach((item) => {
+            if (item.selected) {
+                const itemForDisplay = { ...item, type: "meals" };
+                if (item.numberOfPeople) {
+                    itemForDisplay.numberOfPeople = item.numberOfPeople;
+                }
+                items.push(itemForDisplay);
+            }
+        });
+        return items;
     };
 
     const items = getItemsFromTotalCost();
